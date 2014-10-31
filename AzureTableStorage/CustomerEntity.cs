34b785ba1f1ff3f6bd080c44fdc152e7e2d4685f
Divya-Mangotra <divyam@cloudthat.in>
2014-10-31 18:33:24 +0530

    new project

---
+++ AzureTableStorage/CustomerEntity.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Table;
+
+
+
+namespace AzureTableStorage
+{
+
+    public class CustomerEntity : TableEntity
+    {
+        public CustomerEntity(string lastName, string firstName)
+        {
+            this.PartitionKey = lastName;
+            this.RowKey = firstName;
+        }
+
+        public CustomerEntity() { }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+    
+    
+    
+    
+    /*class CustomerEntity
+    {
+        public class CustomerEntity : TableEntity
+        {
+
+            public CustomerEntity(string firstName, string lastName)
+            {
+                this.PartitionKey = lastName;
+                this.RowKey = firstName;
+            }
+
+
+            public CustomerEntity()
+            {
+            }
+
+            public string Email { get; set; }
+            public string PhoneNumber { get; set; }
+
+        }
+    }*/
+}
---
+++ AzureTableStorage/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace AzureTableStorage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=tableeg;AccountKey=r0PEVAtFwFdl5P3Zl21obz0tfNNsSwhdHAUNSCHCmjKcqnLXD39Xmo2TVnOaIrrOnvgySN5SoEenaNImO8OWmg==");
+            
+            CloudTableClient tableclient = cloudStorageAccount.CreateCloudTableClient();
+
+            CloudTable table = tableclient.GetTableReference("people1");
+            table.CreateIfNotExists();
+
+
+            Console.WriteLine(table.Uri.ToString());
+
+
+
+            CustomerEntity customer2 = new CustomerEntity("Rahul","Sharma");
+            customer2.Email = "rahuls@gmail.com";
+            customer2.PhoneNumber = "9199677888";
+         
+
+            TableOperation insertoperation = TableOperation.Insert(customer2);
+            table.Execute(insertoperation);
+            insertoperation = TableOperation.InsertOrReplace(customer2);
+
+            Console.WriteLine("Entity inserted!");
+
+          
+
+
+
+        }
+
+       
+
+
+
+        
+    }
+}
